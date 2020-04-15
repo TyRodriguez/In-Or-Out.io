4a2b718e6bc0ed2618fe49264cd64889f515d387
Jay Kim <jaykim92@gmail.com>
2020-04-14 21:20:09 -0700

    nav bar and more food preference options

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-var instance = M.Dropdown.init($(".dropdown-trigger"), {coverTrigger:false}, {hover:true});
+$(document).ready(function(){
+    var instance = M.Dropdown.init($(".dropdown-trigger"), {hover:true, coverTrigger:false});
 
-$("")
+    $('#dropdown1').on('click', 'li', function(){
+        $('.dropdown-trigger').text($(this).text())
+    })
+})
+
